780f417f3b7a70305a0462de21e531213f614bd4
Keerthana Nemili <knemili@stradaji.com>
2024-03-25 14:09:34 -0400

    Initial commit

---
+++ .tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var myCustomVisual19E77754A6C124CDE89AEB3C8DCF8DA28: IVisualPlugin;
+export default myCustomVisual19E77754A6C124CDE89AEB3C8DCF8DA28;
---
+++ .tmp/build/src/visual.d.ts
@@ -0,0 +1,10 @@
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+export declare class Visual implements IVisual {
+    private target;
+    private textNode;
+    constructor(options: VisualConstructorOptions);
+    update(options: VisualUpdateOptions): void;
+}
